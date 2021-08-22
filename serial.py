b01c3fb99a95740d0a111d0da15a3afa450e0490
John E. Fisher (Jef) <fishe545@gmail.com>
2021-08-22 12:45:23 +0800

    feat: created init, generate, and reset methods for class SerialGenerator

--- serial.py
+++ serial.py
@@ -4,7 +4,7 @@ class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
     
     >>> serial = SerialGenerator(start=100)
-
+    
     >>> serial.generate()
     100
 
@@ -19,4 +19,16 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start):
+        """ Create a generator from starting from start """
+        self.start = start
+        self.orignal_value = start
 
+    def generate(self):
+        """ Increment start by 1 """
+        self.start += 1
+        return self.start
+    
+    def reset(self):
+        """ Reset start to original value """
+        self.start = self.orignal_value
